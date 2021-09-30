a896656dfe313b3b6f64908639bed4475a8afd3d
Dseta823543 <kqa2128@gmail.com>
2021-09-30 01:51:53 -0500

    Add form and list

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -3,10 +3,34 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
+  patients = [
+    {
+      id:1,
+      name: 'Mario',
+      birth_date: "14-11-99",
+      address:"Calle Saturno"
+    },
+    {
+      id:2,
+      name: 'Valery',
+      birth_date: "15-03-00",
+      address:"Calle Venus"
+
+    },
+    {
+      id:3,
+      name: 'Juan',
+      birth_date: "22-08-96",
+      address:"Calle Pluton"
+    }
+  ];
 
   constructor() {}
 
+  ngOnInit() {}
+
+  getPatientList() {}
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
 })
 export class Tab2Page {
+  patientForm: FormGroup;
 
-  constructor() {}
+  constructor(private router: Router, private formBuilder: FormBuilder) {
+    this.patientForm = this.formBuilder.group({
+      id: [''],
+      name: ['', Validators.required],
+      birth_date: [null],
+      height: [''],
+      address: [null],
+      gps_address: [null],
+    });
+  }
 
+  ngOnInit() {}
 }
